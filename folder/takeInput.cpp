52ce9cac5eab6404b12f58c6b1191dad341b0be7
harshitchopra7 <chopraharshit2000@gmail.com>
2024-01-26 12:32:33 +0530

    take input in array snippet

---
+++ folder/takeInput.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // take n integers in array 
+    int n;
+    cin>>n;
+
+    int arr[n];
+    
+    for(int i=0; i<n; i++) {
+        cin>>arr[i];
+    }
+
+    return 0;
+}
