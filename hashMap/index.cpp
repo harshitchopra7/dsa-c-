ac181da3684b076ac9e15ba0960a6f5e5b36b673
harshitchopra7 <chopraharshit2000@gmail.com>
2024-02-28 20:07:22 +0530

    ordered hash map

---
+++ hashMap/index.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // syntax - ordered map
+    // map<key, value>
+    map<int, int> mpp;
+
+    for (int i = 0; i < n; i++)
+    {
+        mpp[arr[i]]++;
+    }
+
+    for (auto it : mpp)
+    {
+        cout << it.first << " => " << it.second << endl;
+    }
+
+    return 0;
+}
